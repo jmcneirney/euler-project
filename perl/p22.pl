bcdc595f0727efa13d7c400a5ca60369a7db5412
Joe McNeirney <jmcneirney@gmail.com>
2018-04-11 06:04:19 -0700

    problem22 in perl5

--- perl/p22.pl
+++ perl/p22.pl
@@ -10,4 +10,30 @@
 # 
 # What is the total of all the name scores in the file?
 
+use v5.26;
+use strict;
+use warnings;
+use Carp;
+use Readonly;
 
+Readonly $ALPHABETIC_BASE => 96;
+
+use Data::Dumper;
+
+open( my $NAMES, '<', './p022_names.txt' ) or croak "Unable to read file";
+
+my %names;
+my $position = 1;
+foreach my $name ( sort split(',', <$NAMES> ) ) {
+    $name =~ s/"//g;
+    my $val;
+    map { $val += ord($_) - $ALPHABETIC_BASE } split('', lc($name));
+    $names{$name} = $val * $position;
+    $position++;
+}
+
+my $total;
+foreach my $name_val ( values %names ) {
+    $total += $name_val;
+}
+say $total;
