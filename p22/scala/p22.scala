a6a29cc80f355bcbdfc33946a4ee349be4b4481b
Joe McNeirney <jmcneirney@gmail.com>
2018-04-16 06:28:23 -0700

    comments changes - scala io

--- p22/scala/p22.scala
+++ p22/scala/p22.scala
@@ -1,13 +1,23 @@
-# Names scores
-# Problem 22
-# 
-# Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, 
-# begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value 
-# by its alphabetical position in the list to obtain a name score.
-# 
-# For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th 
-# name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
-# 
-# What is the total of all the name scores in the file?
+// Names scores
+// Problem 22
+// 
+// Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, 
+// begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value 
+// by its alphabetical position in the list to obtain a name score.
+// 
+// For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th 
+// name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+// 
+// What is the total of all the name scores in the file?
 
+import scala.io.Source._;
 
+object NameToSum {
+
+    def main(args:Array[String]): Unit = {
+// probablly add some exception handling all up in heres
+        val names = fromFile("../p022_names.txt")
+        println( "Hello " + names )
+ println( "one two three" );
+    }
+}
