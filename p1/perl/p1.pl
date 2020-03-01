30f545a06d1ed8fba9dbc729dbe73d90fa2991ba
Joe McNeirney <jmcneirney@gmail.com>
2020-03-01 07:47:59 -0800

    problem1 removing hard coded num

--- p1/perl/p1.pl
+++ p1/perl/p1.pl
@@ -1,14 +1,15 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
-# The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+# The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below $less_than_value.
 
 use List::Util qw(sum);
+my $less_than_val = 1000;
 
-my @list = (1..1000);
+my @list = (1..$less_than_val);
 
-my $sum1 = sum( grep {($_ % 3 == 0 || $_ % 5 == 0) && $_ < 1000} @list ) . "\n";
+my $sum1 = sum( grep {($_ % 3 == 0 || $_ % 5 == 0) && $_ < $less_than_val} @list ) . "\n";
 
 # Or, alternatively w/o List::Util
-my @list_3_5 =  grep {($_ % 3 == 0 || $_ % 5 == 0) && $_ < 1000} @list;
+my @list_3_5 =  grep {($_ % 3 == 0 || $_ % 5 == 0) && $_ < $less_than_val} @list;
 my $sum2;
 foreach my $add_me ( @list_3_5 ) {
     $sum2 += $add_me;
@@ -16,6 +17,6 @@ foreach my $add_me ( @list_3_5 ) {
 
 # or
 my $sum3;
-map { $sum3 += $_ } grep {($_ % 3 == 0 || $_ % 5 == 0) && $_ < 1000} @list;
+map { $sum3 += $_ } grep {($_ % 3 == 0 || $_ % 5 == 0) && $_ < $less_than_val} @list;
 print "$sum3\n" if( ($sum1 == $sum2) && ( $sum2 == $sum3 ) );
 
