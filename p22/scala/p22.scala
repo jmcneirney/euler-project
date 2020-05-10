547f16f11d3dbd78bc7b201f9d950941f2195e03
Joe McNeirney <jmcneirney@gmail.com>
2020-05-10 05:32:57 -0700

    promlem22-scala removing a map()

--- p22/scala/p22.scala
+++ p22/scala/p22.scala
@@ -1,9 +1,24 @@
+//  Names scores
+//  Problem 22
+//  
+//  Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, 
+//  begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value 
+//  by its alphabetical position in the list to obtain a name score.
+//  
+//  For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th 
+//  name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+//  
+//  What is the total of all the name scores in the file?
+
+
 import scala.io.Source
 
 object p22 {
 
    val strip_quotes  = (name: String) => name.stripPrefix("\"").stripSuffix("\"");
-   val chars_to_nums = (name: String) => name.toCharArray().map( _.toByte.toInt - 64 )
+   val sum_chars_to_nums = (name: String) => {
+       name.toCharArray().map( _.toByte.toInt - 64 ).sum
+   }
 
    val file  = Source.fromFile("./p022_names.txt")
    val names = file.mkString
@@ -12,16 +27,12 @@ object p22 {
    
    val names_to_nums = 
         name_list.map( strip_quotes ).sorted
-                 .map( chars_to_nums )  /// this repeated use of map seems bad ... idk
-                 .map( _.sum )
+                 .map( sum_chars_to_nums )
                  .zipWithIndex
                  .map( i => { i._1 * (i._2 + 1) } )
 
    val total = names_to_nums.sum
-                 
-   // You're going through the list and stripping the quotes
-   // Then you going through the list and blah blah blah
-   //  Seems like you wouldn't want to go through the list more 
-   //  than once. Do you want to do everything you need to do on one pass?
-   
+
+   println( total )
 }
+
