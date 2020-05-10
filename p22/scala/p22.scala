d976c9d343671af9481d58f3a757e7e6f0046e22
Joe McNeirney <jmcneirney@gmail.com>
2020-05-10 05:41:18 -0700

    problem22-scala removing map()

--- p22/scala/p22.scala
+++ p22/scala/p22.scala
@@ -1,13 +1,13 @@
 //  Names scores
 //  Problem 22
-//  
-//  Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, 
-//  begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value 
+//
+//  Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names,
+//  begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value
 //  by its alphabetical position in the list to obtain a name score.
-//  
-//  For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th 
+//
+//  For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th
 //  name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
-//  
+//
 //  What is the total of all the name scores in the file?
 
 
@@ -24,8 +24,8 @@ object p22 {
    val names = file.mkString
 
    val name_list = names.split(",")
-   
-   val names_to_nums = 
+
+   val names_to_nums =
         name_list.map( strip_quotes ).sorted
                  .map( sum_chars_to_nums )
                  .zipWithIndex
